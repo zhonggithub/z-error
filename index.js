29c2ddce756065b3f1e199dd9dc5a06166a7275b
Zz <zhi.zhao@sunlight-tech.com>
2016-09-05 20:10:31 +0800

    增加代码

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 module.exports = require('./lib/ZError');
-module.exports.errorCode2Text = require('./lib/errorCodeTable').errorCode2Text;
-module.exports.formatParam2Code = require('./lib/errorCodeTable').formatParam2Code;
+module.exports.errorCode2Text = require('./lib/errorTable').errorCode2Text;
+module.exports.formatParam2Code = require('./lib/errorTable').formatParam2Code;
