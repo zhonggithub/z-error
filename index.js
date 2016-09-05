193123d1f994e52f5e8b5917dde36ce330ff064a
Zz <quitjie@gmail.com>
2016-09-05 22:36:35 +0800

    增加代码

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 module.exports = require('./lib/ZError');
 module.exports.errorCode2Text = require('./lib/errorTable').errorCode2Text;
 module.exports.formatParam2Code = require('./lib/errorTable').formatParam2Code;
+module.exports.getMessage = require('./lib/errorTable').getMessage;
--- test/test.js
+++ test/test.js
@@ -2,8 +2,13 @@
 let ZError = require('../index');
 let errorCode2Text = require('../index').errorCode2Text;
 let formatParam2Code = require('../index').formatParam2Code;
+let getMessage = require('../index').getMessage;
 
-let error = new ZError("erro", 1030);
+let error = new ZError("erro", 1001, 'zh-cn');
 console.log(error.name);
-console.log(errorCode2Text(1050));
+console.log(error.getMessage());
+console.log(error.getMessage('en'));
+console.log(error.stack);
+console.log(errorCode2Text(1001));
 console.log(formatParam2Code('accout'));
+console.log(getMessage(1001, 'zh-cn'));
