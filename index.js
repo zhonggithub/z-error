37ce3b4fbf7349ee3d84194263ceeec37226f44c
Zz <quitjie@gmail.com>
2016-09-05 23:23:17 +0800

    增加API

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports = require('./lib/ZError');
 module.exports.errorCode2Text = require('./lib/errorTable').errorCode2Text;
 module.exports.formatParam2Code = require('./lib/errorTable').formatParam2Code;
 module.exports.getMessage = require('./lib/errorTable').getMessage;
+module.exports.verify = require('./lib/functionPool').verify;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ let ZError = require('../index');
 let errorCode2Text = require('../index').errorCode2Text;
 let formatParam2Code = require('../index').formatParam2Code;
 let getMessage = require('../index').getMessage;
+let verify = require('../index').verify;
 
 let error = new ZError("erro", 1001, 'zh-cn');
 console.log(error.name);
@@ -12,3 +13,8 @@ console.log(error.stack);
 console.log(errorCode2Text(1001));
 console.log(formatParam2Code('accout'));
 console.log(getMessage(1001, 'zh-cn'));
+let err = verify({account: 'aa'}, ['password']);
+console.log(err);
+console.log(err.getMessage('zh-cn'));
+err.local = 'zh-cn';
+console.log(err.getMessage());
