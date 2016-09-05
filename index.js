ac14911320fe46499ffd6b419ef9c6b7ae49dc14
Zz <zhi.zhao@sunlight-tech.com>
2016-09-05 10:11:24 +0800

    修改代码，增加测试用例

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 'use strict';
 module.exports = require('./lib/ZError');
+module.exports.errorCode2Text = require('./lib/errorCodeTable').errorCode2Text;
+module.exports.formatParam2Code = require('./lib/errorCodeTable').formatParam2Code;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 'use strict';
 let ZError = require('../index');
+let errorCode2Text = require('../index').errorCode2Text;
+let formatParam2Code = require('../index').formatParam2Code;
 
-let error = new ZError("erro");
+let error = new ZError("erro", 1030);
 console.log(error.name);
+console.log(errorCode2Text(1050));
+console.log(formatParam2Code('accout'));
