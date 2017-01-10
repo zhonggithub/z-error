254ba381bdf32fd09877b0924566a0553167dc5a
Zz <zhi.zhao@sunlight-tech.com>
2017-01-10 10:36:53 +0800

    修改代码00

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
-module.exports = require('./lib/ZError');
-module.exports.errorCode2Text = require('./lib/errorTable').errorCode2Text;
-module.exports.formatParam2Code = require('./lib/errorTable').formatParam2Code;
-module.exports.getMessage = require('./lib/errorTable').getMessage;
-module.exports.verify = require('./lib/functionPool').verify;
+const ZError = require('./lib/ZError');
+const errorCode2Text = require('./lib/errorTable').errorCode2Text;
+const formatParam2Code = require('./lib/errorTable').formatParam2Code;
+const getMessage = require('./lib/errorTable').getMessage;
+const verify = require('./lib/functionPool').verify;
+
+module.exports = { ZError, errorCode2Text, formatParam2Code, getMessage, verify };
