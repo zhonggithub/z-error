1ed75290cf4e05f81a40c00e690261e58223be9f
zhonggithub <1006817093@qq.com>
2017-01-15 10:54:48 +0800

    修改代码

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const errorCode2Text = require('./lib/errorTable').errorCode2Text;
 const formatParam2Code = require('./lib/errorTable').formatParam2Code;
 const getMessage = require('./lib/errorTable').getMessage;
 const verify = require('./lib/functionPool').verify;
+const setLocal = require('./lib/functionPool').setLocal;
 
-module.exports = { ZError, errorCode2Text, formatParam2Code, getMessage, verify };
+module.exports = { ZError, errorCode2Text, formatParam2Code, getMessage, verify, setLocal };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 'use strict';
-const ZError = require('../index');
+const ZError = require('../index').ZError;
 const errorCode2Text = require('../index').errorCode2Text;
 const formatParam2Code = require('../index').formatParam2Code;
 const getMessage = require('../index').getMessage;
 const verify = require('../index').verify;
+const setLocal = require('../index').setLocal;
+setLocal('zh-cn');
 
-let error = new ZError("erro", 1001, 'zh-cn');
+let error = new ZError("erro", 400, 1001);
 console.log(error.name);
 console.log(error);
 console.log(error.getMessage());
@@ -18,4 +20,4 @@ let err = verify({account: 'aa'}, ['password']);
 console.log(err);
 console.log(err.getMessage('zh-cn'));
 err.local = 'zh-cn';
-console.log(err.getMessage());
+console.log(err.getMessage())
